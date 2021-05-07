8045055597c24d5463c5dbcb84667f61375e15e1
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2021-05-07 17:28:45 -0300

    Add Fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
+import {
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fontes';
 
 export function Welcome() {
   return (
@@ -12,15 +20,20 @@ export function Welcome() {
         suas plantas {'\n'}
         de forma fácil
 			</Text>
-      <Image source={wateringImg} style={styles.image} />
+      <Image
+        source={wateringImg}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
       <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>
-          >
-        </Text>
+        <Feather
+          name="chevron-right"
+          style={styles.buttonIcon}
+        />
       </TouchableOpacity>
 
     </SafeAreaView>
@@ -31,20 +44,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 38
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7
   },
   button: {
     backgroundColor: colors.green,
@@ -55,13 +74,9 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56
   },
-  image: {
-    width: 292,
-    height: 284
-  },
-  buttonText: {
+  buttonIcon: {
     color: colors.white,
-    fontSize: 24
+    fontSize: 32
   }
 });
 
